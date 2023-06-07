b9b81de7323db9d8114992dbd7c689ac7643f520
I-M-Marinov <lcfrrr@gmail.com>
2023-06-07 18:45:13 -0500

    P2. Gauss' Trick solved
    P2. Gauss' Trick solved

---
+++ P2.Gauss'Trick/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P2.Gauss_Trick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputInts =Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int startupLength = inputInts.Count;
+
+            for (int i = 0; i < startupLength / 2; i++)
+            {
+                inputInts[i] += inputInts[inputInts.Count - 1];
+
+                inputInts.RemoveAt(inputInts.Count - 1);
+            }
+            Console.WriteLine(string.Join(" ", inputInts));
+        }
+    }
+}
