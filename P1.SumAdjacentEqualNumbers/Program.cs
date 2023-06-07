fb1e695d8df20ef37c13202bdc78496401e4e718
I-M-Marinov <lcfrrr@gmail.com>
2023-06-07 17:45:20 -0500

    Add project files.

---
+++ P1.SumAdjacentEqualNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P1.SumAdjacentEqualNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = Console.ReadLine().Split().Select(double.Parse).ToList();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (i + 1 >= numbers.Count)
+                {
+                    break;
+                }
+                if (numbers[i] == numbers[i + 1])
+                {
+                    numbers[i] += numbers[i + 1];
+                    numbers.RemoveAt(i+1);
+                    i = -1;
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
