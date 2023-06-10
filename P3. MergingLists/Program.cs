07852336f1abf43a16790093b29b041c39bcdb7d
I-M-Marinov <lcfrrr@gmail.com>
2023-06-10 18:03:39 -0500

    Update Program.cs
    Solution to P3. Merging Lists

--- P3. MergingLists/Program.cs
+++ P3. MergingLists/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace P3._MergingLists
 {
@@ -6,7 +8,39 @@ namespace P3._MergingLists
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> listOfInts1 = Console.ReadLine().Split().Select(int.Parse).ToList();  
+            List<int> listOfInts2 = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> resultList = new List<int>();
+            int list1Length = listOfInts1.Count;
+            int list2Length = listOfInts2.Count;
+
+            for (int i = 0; i < list1Length; i++)
+            {
+                resultList.Add(listOfInts1[i]);
+
+                if (i < list2Length)
+                {
+                    resultList.Add(listOfInts2[i]);
+                }
+            }
+            for (int i = list1Length; i < list2Length; i++)
+            {
+                resultList.Add(listOfInts2[i]);
+            }
+
+
+            //if (list1Length > list2Length)
+            //{
+            //    for (int i = list2Length; i < list1Length; i++)
+            //    {
+            //        resultList.Add(listOfInts1[i]);
+            //    }
+            //}
+            //else
+            //{
+
+            //}
+            Console.WriteLine(string.Join(" ", resultList));   
         }
     }
 }
